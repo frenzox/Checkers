51be2007ee69924f9212148be8eb04a54e2488d7
frenzox <frenzox@msn.com>
2014-05-13 16:18:57 -0300

    Header comment

--- src/br/pucpr/Main.java
+++ src/br/pucpr/Main.java
@@ -1,3 +1,13 @@
+/* Checkers with Alfa-Beta pruning
+ * 
+ * Developed by:
+ * Guilherme Silva (frenzox@msn.com)
+ * André Luppi (andrell_alemao@hotmail.com)
+ * 
+ * Pontifical Catholic University of Paraná - Curitiba/Paraná/Brazil
+ * 
+ * Project repository: https://github.com/frenzox/Checkers/
+ */
 package br.pucpr;
 
 public class Main
