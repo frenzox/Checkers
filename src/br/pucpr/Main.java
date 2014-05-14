4fb28b510c4938dc0d4fbe529b10e38eb5fa8edc
frenzox <frenzox@msn.com>
2014-05-14 09:32:45 -0300

    Buttons implementation

--- src/br/pucpr/Main.java
+++ src/br/pucpr/Main.java
@@ -10,44 +10,107 @@
  */
 package br.pucpr;
 
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import br.pucpr.Pieces.Pawn;
 
 public class Main
 {
 	private boolean flag = false;
 	private static IPiece table[][];
+	static JButtonCheckers[][] buttons;
 
-	public static void main()
+	public static void main( String[] args )
 	{
+		try
+		{
+			UIManager.setLookAndFeel( UIManager
+					.getCrossPlatformLookAndFeelClassName() );
+		} catch ( UnsupportedLookAndFeelException e )
+		{
+
+		} catch ( ClassNotFoundException e )
+		{
+
+		} catch ( InstantiationException e )
+		{
+
+		} catch ( IllegalAccessException e )
+		{
+
+		}
+
+		ImageIcon imgTst = new ImageIcon( "/Users/Guilherme/Desktop/tst.png" );
+		
+
+		buttons = new JButtonCheckers[8][8];
+
+		final JFrame frame = new JFrame( "Damas" );
+		frame.setSize( 800, 800 );
+		frame.setLocationRelativeTo( null );
+		frame.setLayout( null );
+
+		JPanel contentPane = new JPanel();
+		contentPane.setLayout( new GridLayout( 8, 8 ) );
+		// contentPane.setLocation( 0, 0 );
+		contentPane.setBounds( 0, 0, 600, 600 );
+		frame.add( contentPane );
+
 		for ( int i = 0; i < 8; i++ )
 			for ( int j = 0; j < 8; j++ )
 			{
+				JButtonCheckers btn = new JButtonCheckers( i, j );
+				btn.setIcon( imgTst );
+				buttons[i][j] = btn;
+				if ( ( i + j ) % 2 != 0 )
+					buttons[i][j].setBackground( Color.black );
 
-				if ( i + j % 2 != 0 )
-				{
+				else
+					buttons[i][j].setBackground( Color.white );
+				buttons[i][j].setSize( 10, 10 );
 
-					if ( j <= 2 )
+				contentPane.add( btn );
+
+				frame.setVisible( true );
+
+				/*
+				for ( i = 0; i < 8; i++ )
+					for ( j = 0; j < 8; j++ )
 					{
 
-						table[i][j] = new Pawn();
-						table[i][j].setX0( i );
-						table[i][j].setY0( j );
-						table[i][j].setColor( Colors.BLACK );
+						if ( i + j % 2 != 0 )
+						{
 
-					}
-					if ( j >= 5 )
-					{
+							if ( j <= 2 )
+							{
 
-						table[i][j] = new Pawn();
-						table[i][j].setX0( i );
-						table[i][j].setY0( j );
-						table[i][j].setColor( Colors.WHITE );
+								table[i][j] = new Pawn();
+								table[i][j].setX0( i );
+								table[i][j].setY0( j );
+								table[i][j].setColor( Colors.BLACK );
 
-					}
+							}
+							if ( j >= 5 )
+							{
 
-				}
+								table[i][j] = new Pawn();
+								table[i][j].setX0( i );
+								table[i][j].setY0( j );
+								table[i][j].setColor( Colors.WHITE );
 
+							}
+
+						}
+					}*/
 			}
+
 	}
 
 	public int movimenta( int xOr, int yOr, int xDest, int yDest )
